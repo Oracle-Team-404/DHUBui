9d1f6c35c6cf1638524425ae20e36154d5d8d0c8
dspires <dspires@DSPIRES-US.us.oracle.com>
2017-07-26 13:12:08 -0500

    client updates

--- DHUD-client/src/app/app.module.ts
+++ DHUD-client/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app.routing.module'
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { PresentComponent } from './present/present.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    PresentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
